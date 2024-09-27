56b9e1db24be32d9e26dfb6478a105af521ab852
yunsammy <yun3.jo@gmail.com>
2024-09-27 23:14:10 +0900

    Refactor: 일부 조건 순서 및 포함관계 재구성 & 플레이어 Hit 없는것으로 가정

--- week4/blackjack.js
+++ week4/blackjack.js
@@ -5,7 +5,7 @@ let cardOneBank = getRandomCardNum();
 let cardTwoBank = getRandomCardNum();
 
 function getRandomCardNum() {
-  return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 11);
 }
 
 // 딜러는 17점 이상일 때 멈춰야 하고, 그 이하일 때는 추가 카드를 뽑아야 함
@@ -14,49 +14,43 @@ while (bankSum < 17) {
   bankSum += getRandomCardNum();
 }
 
-// 플레이어가 21점 달성하면 블랙잭(즉시 승리)
-if (sum === 21) {
-  console.log("You win");
-  process.exit();
-}
-
-let cardThree = getRandomCardNum();
-sum += cardThree;
+// 플레이어 추가 카드(Hit)는 뽑지 않는 것으로 구현 (항상 2장으로 승부)
+// let cardThree = getRandomCardNum();
+// sum += cardThree;
 
 function blackjack(playerSum, bankSum) {
   let isPlayerWin;
+  let isDraw;
   // 플레이어가 21점 달성하면 블랙잭(즉시 승리)
   if (playerSum == 21) {
     isPlayerWin = true;
   }
-
   // 플레이어가 21점 초과 -> 패배
-  if (playerSum > 21) {
+  else if (playerSum > 21) {
     isPlayerWin = false;
   }
-
-  // 딜러가 21점 초과 -> 승리
-  if (bankSum > 21) {
+  // 플레이어가 21점 초과하지 않고 딜러가 21점 초과 -> 승리
+  else if (bankSum > 21) {
     isPlayerWin = true;
   }
-
-  // 플레이어와 딜러의 카드 합계가 21을 넘으면 Bust(패배)
-  if (playerSum + bankSum > 21) {
+  // 플레이어, 딜러 모두 21점 초과하지 않은 경우
+  // 플레이어가 딜러보다 카드 합계 높은 경우 -> 승리
+  else if (sum > bankSum) {
+    isPlayerWin = true;
+  }
+  // 플레이어보다 딜러가 카드 합계 높은 경우 -> 패배
+  else if (sum < bankSum) {
     isPlayerWin = false;
   }
-
-  if (playerSum === bankSum) {
-    console.log("Draw");
-    return;
-  }
-
-  // 플레이어 패의 숫자가 딜러 크기보다 큰 경우
-  if (playerSum > bankSum) {
-    isPlayerWin = true;
+  // 플레이어와 딜러의 카드 합계 같은 경우 -> 무승부
+  else if (playerSum === bankSum) {
+    isDraw = true;
   }
 
   if (isPlayerWin) {
     console.log("You win");
+  } else if (isDraw) {
+    console.log("Draw");
   } else {
     console.log("You lost");
   }
